a0dfc974e9041399089f752c8ba0b7ada2e0d6d2
Alphonse <alphonsemehounme9@gmail.com>
2024-06-30 15:58:49 -0700

    Add get user on users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,8 +7,8 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views("/users", methods=['GET'], strict_slashes=False)
-def get_user():
+@app_views.route("/users", methods=['GET'], strict_slashes=False)
+def get_users():
     """
     Method to get a user
     """
@@ -17,3 +17,14 @@ def get_user():
     for key, value in users_json.items():
         users.update({key: value.to_dict()})
     return jsonify(users)
+
+@app_views.route("/users/<user_id>", methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """
+    Method to get a user
+    """
+    user_obj = storage.get(User, user_id)
+    if user_obj:
+        return jsonify(user_obj.to_dict())
+    else:
+        abort(404)
