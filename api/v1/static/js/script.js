5cc35cb78e08204aa0c0b3cb33570c72de6cc035
Alphonse <alphonsemehounme9@gmail.com>
2024-07-14 13:37:25 -0700

    Update button at verify

--- api/v1/static/js/script.js
+++ api/v1/static/js/script.js
@@ -43,11 +43,44 @@ document.addEventListener('DOMContentLoaded', function() {
 		console.log(content)
 		if (content == False) {
 			$("#status1").text("validated");
+			$("#status1").css("color", "green")
+		}
+		else {
+			$("#status1").text("Failed");
+			$("#status1").css("color", "red")
 		}
             },
             error: function(xhr, status, error) {
                 console.error(`Error fetching file: ${xhr.status} ${xhr.statusText}`);
             }
         });
+    });
+
+    $('#verify2').on('click', function() {
+	const owner = "alphonsemehounme";
+	const url = `https://api.github.com/repos/${owner}/bitdev/contents/chapter1/answer2`;
+
+	$.ajax({
+	    url: url,
+	    method: "GET",
+	    headers: {
+		"Accept": "application/vnd.github.v3+json"
+	    },
+	    success: function(response) {
+		const content = atob(response.content);
+		console.log(content)
+		if (content == 1) {
+			$("#status2").text("Validated")
+			$("#status2").css("color", "green")
+		}
+		else {
+			$("#status2").text("Failed")
+			$("#status2").css("color", "red")
+		}
+	    },
+	    error: function(xhr, status, error) {
+		    console.error(`Error fetching file: ${xhr.status} ${xhr.statusText}`);
+	    }
+	});
     });
 });
